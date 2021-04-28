c652618d91dfa96b69eb8475e218930f4a2c6564
Michaël Zasso <targos@protonmail.com>
2021-04-28 13:23:37 +0200

    fix: handle empty strings

--- src/stack-error.js
+++ src/stack-error.js
@@ -1,9 +1,14 @@
 export default class StackError extends Error {
   constructor(message = '') {
-    const words = message.split(/\s+/);
+    const words = message.split(/\s+/).filter((word) => word !== '');
     const previousLimit = Error.stackTraceLimit;
+
     Error.stackTraceLimit = words.length;
     try {
+      if (words.length === 0) {
+        throw new Error('❌');
+      }
+
       const funcs = words.reverse().map((word) => {
         const func = new Function(
           'funcs',
@@ -20,12 +25,8 @@ export default class StackError extends Error {
         return func;
       });
 
-      if (funcs.length > 0) {
-        const func = funcs[0];
-        func(funcs.slice(1));
-      } else {
-        capture();
-      }
+      const func = funcs[0];
+      func(funcs.slice(1));
     } catch (error) {
       Object.setPrototypeOf(error, StackError.prototype);
       return error;
