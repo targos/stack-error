2cccd6f7f8eb242f8030c3dfb51aecc57fb07e5f
Michaël Zasso <targos@protonmail.com>
2021-04-28 13:26:56 +0200

    test: add lorem ipsum test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+import { LoremIpsum } from 'lorem-ipsum';
+
 import StackError from './src/stack-error.js';
 
 function test(message) {
@@ -11,3 +13,4 @@ test('A B C');
 test('A B C D E F G H I J K L M N O P Q R S T U V W X Y Z');
 test('A  B');
 test('HU " HU');
+test(new LoremIpsum().generateWords(100));
