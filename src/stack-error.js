343c92255e62f9ead57793be4aa34caa9632846b
Mestery <mestery@pm.me>
2021-04-28 14:11:52 +0200

    force error's name and stack
    also remove the usage of `new Function`

--- src/stack-error.js
+++ src/stack-error.js
@@ -10,25 +10,24 @@ export default class StackError extends Error {
       }
 
       const funcs = words.reverse().map((word) => {
-        const func = new Function(
-          'funcs',
-          `
-        if (funcs.length > 0) {
-          const func = funcs[0];
-          func(funcs.slice(1));
-        } else {
-          throw new Error("↓");
-        }
-      `,
-        );
-        Object.defineProperty(func, 'name', { value: word });
-        return func;
+        return {
+          [word](funcs) {
+            if (funcs.length > 0) {
+              const func = funcs[0];
+              func(funcs.slice(1));
+            } else {
+              throw new Error("↓");
+            }
+          }
+        }[word];
       });
 
       const func = funcs[0];
       func(funcs.slice(1));
     } catch (error) {
       Object.setPrototypeOf(error, StackError.prototype);
+      error.name = 'StackError';
+      error.stack = error.stack.split('\n', words.length + 1).join('\n');
       return error;
     } finally {
       Error.stackTraceLimit = previousLimit;
